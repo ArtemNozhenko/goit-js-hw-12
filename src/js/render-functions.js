b126c89f2f1dd281269a21efff0512de2d3129c2
ArtemNozhenko <148395424+ArtemNozhenko@users.noreply.github.com>
2024-03-27 22:17:25 +0300

    Initial commit

---
+++ src/js/render-functions.js
@@ -0,0 +1,37 @@
+import { lightbox, galleryList } from '../main';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+export function renderGallery(arr) {
+  if (arr.length == 0) {
+    iziToast.error({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    });
+    return;
+  } else {
+    const markup = arr
+      .map(photo => {
+        return `<li class="gallery-item">
+            <a class="photos-list-link" href="${photo.largeImageURL}">
+            <img class="image" src="${photo.webformatURL}" alt="${photo.tags}"/>
+            </a>
+            <ul class="list-container">
+            <li class="item-container"><p><span class="title">Likes</span></br>${photo.likes}</p></li>
+            <li class="item-container"><p><span class="title">Views</span></br>${photo.views}</p></li>
+            <li class="item-container"><p><span class="title">Comments</span></br>${photo.comments}</p></li>
+            <li class="item-container"><p><span class="title">Downloads</span></br>${photo.downloads}</p></li>
+            </ul>
+            </li>`;
+      })
+      .join('');
+    galleryList.insertAdjacentHTML('beforeend', markup);
+
+    lightbox.refresh();
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,63 @@
+import { getImages } from './js/pixabay-api';
+import { renderGallery } from './js/render-functions';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('.search-input');
+const input = searchInput.value.trim();
+export const galleryList = document.querySelector('.gallery');
+
+export const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+const preloader = document.querySelector('.loader');
+preloader.style.display = 'none';
+
+export const showLoader = () => {
+  preloader.style.display = 'flex';
+};
+const hideLoader = () => {
+  preloader.style.display = 'none';
+};
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  galleryList.innerHTML = '';
+  const input = searchInput.value.trim();
+  if (input !== '') {
+    showLoader();
+    getImages(input)
+      .then(data => {
+        renderGallery(data.hits);
+      })
+      .catch(error => {
+        console.log(error);
+        iziToast.error({
+          message: 'Sorry, an error occurred while loading. Please try again!',
+          theme: 'dark',
+          progressBarColor: '#FFFFFF',
+          color: '#EF4040',
+          position: 'topRight',
+        });
+      })
+      .finally(() => {
+        hideLoader();
+        searchForm.reset();
+      });
+  } else {
+    iziToast.show({
+      message: 'Please complete the field!',
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    });
+  }
+});
